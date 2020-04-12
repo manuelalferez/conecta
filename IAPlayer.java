c319067ab636222e16005157070b021b09420983
manuel <manuelalferezruiz@gmail.com>
2020-04-12 17:07:07 +0200

    se añade método esEmpate y se corrige el método columnaLlena. Sigue dando fallos, la partida termina sin ganador

--- IAPlayer.java
+++ IAPlayer.java
@@ -26,6 +26,8 @@ public class IAPlayer extends Player {
         FILAS = tablero.getFilas();
         COLUMNAS = tablero.getColumnas();
         tablero_copia = tablero.toArray();
+        System.out.println("Inicio:");
+        imprimirTablero();
 
         int mejorJugada = algoritmoMinMax();
         return tablero.checkWin(tablero.setButton(mejorJugada, Conecta4.PLAYER2), mejorJugada, conecta);
@@ -42,60 +44,70 @@ public class IAPlayer extends Player {
     }
 
     private int maximizar(int profundidad) {
-        int mejor_jugada = SIN_JUGADA;
-        int valoracion = PEOR_VALORACION_MAX;
-        int mejor_valoracion = valoracion;
-        int fila;
-        System.out.println("Profundidad: " + profundidad);
-        for (int col = 0; col < COLUMNAS; col++) {
-            if (!columnaLlena(col)) {
-                fila = setFicha(col, Conecta4.PLAYER1);
-                imprimirTablero();
-                int estado_del_juego = checkWin(fila, col);
-                if (estado_del_juego == SIN_GANADOR) {
-                    valoracion = mayorValor(valoracion, minimizar(profundidad++));
-                    if (valoracion > mejor_valoracion) {
-                        mejor_valoracion = valoracion;
-                        mejor_jugada = col;
+        if (esEmpate()) {
+            return 0;
+        } else {
+            int mejor_jugada = SIN_JUGADA;
+            int valoracion = PEOR_VALORACION_MAX;
+            int mejor_valoracion = valoracion;
+            int fila;
+            System.out.println("Profundidad: " + profundidad);
+            for (int col = 0; col < COLUMNAS; col++) {
+                if (!columnaLlena(col)) {
+                    System.out.println("Soy max, Columna: " + col);
+                    fila = setFicha(col, Conecta4.PLAYER1);
+                    imprimirTablero();
+                    int estado_del_juego = checkWin(fila, col);
+                    if (estado_del_juego == SIN_GANADOR) {
+                        valoracion = mayorValor(valoracion, minimizar(profundidad++));
+                        if (valoracion > mejor_valoracion) {
+                            mejor_valoracion = valoracion;
+                            mejor_jugada = col;
+                        }
+                        tablero_copia[fila][col] = 0;
+                    } else {
+                        return estado_del_juego;
                     }
-                    tablero_copia[fila][col] = 0;
-                } else {
-                    return estado_del_juego;
                 }
             }
+            return mejor_jugada;
         }
-        return mejor_jugada;
     }
 
     private int minimizar(int profundidad) {
-        int mejor_jugada = SIN_JUGADA;
-        int valoracion = PEOR_VALORACION_MIN;
-        int mejor_valoracion = valoracion;
-        int fila;
-        System.out.println("Profundidad: " + profundidad);
-        for (int col = 0; col < COLUMNAS; col++) {
-            if (!columnaLlena(col)) {
-                fila = setFicha(col, Conecta4.PLAYER2);
-                imprimirTablero();
-                int estado_del_juego = checkWin(fila, col);
-                if (estado_del_juego == SIN_GANADOR) {
-                    valoracion = menorValor(valoracion, maximizar(profundidad++));
-                    if (valoracion < mejor_valoracion) {
-                        mejor_valoracion = valoracion;
-                        mejor_jugada = col;
+        if (esEmpate()) {
+            return 0;
+        } else {
+            int mejor_jugada = SIN_JUGADA;
+            int valoracion = PEOR_VALORACION_MIN;
+            int mejor_valoracion = valoracion;
+            int fila;
+            System.out.println("Profundidad: " + profundidad);
+            for (int col = 0; col < COLUMNAS; col++) {
+                if (!columnaLlena(col)) {
+                    System.out.println("Soy min, Columna: " + col);
+                    fila = setFicha(col, Conecta4.PLAYER2);
+                    imprimirTablero();
+                    int estado_del_juego = checkWin(fila, col);
+                    if (estado_del_juego == SIN_GANADOR) {
+                        valoracion = menorValor(valoracion, maximizar(profundidad++));
+                        if (valoracion < mejor_valoracion) {
+                            mejor_valoracion = valoracion;
+                            mejor_jugada = col;
+                        }
+                        tablero_copia[fila][col] = 0;
+                    } else {
+                        return estado_del_juego;
                     }
-                    tablero_copia[fila][col] = 0;
-                } else {
-                    return estado_del_juego;
                 }
             }
+            System.out.println(mejor_jugada);
+            return mejor_jugada;
         }
-        System.out.println(mejor_jugada);
-        return mejor_jugada;
     }
 
     private boolean columnaLlena(int col) {
-        return tablero_copia[FILAS - 1][col] != 0;
+        return tablero_copia[0][col] != 0;
     }
 
     private int setFicha(int col, int jugador) {
@@ -104,18 +116,18 @@ public class IAPlayer extends Player {
             fila--;
         }
         tablero_copia[fila][col] = jugador;
+        System.out.println("Pongo ficha");
         return fila;
     }
 
-    public int ponerFicha(int col, int jugador) {
-        int y = FILAS - 1;
-        //Buscamos la fila donde colocar la pieza
-        while ((y >= 0) && (tablero_copia[y][col] != 0)) {
-            y--;
+    public boolean esEmpate() {
+        for (int i = 0; i < COLUMNAS; i++) {
+            if (tablero_copia[0][i] == 0) {
+                return false;
+            }
         }
-        tablero_copia[y][col] = jugador; //Colocamos la ficha
-        return y;
-    } // ponerFicha
+        return true;
+    }
 
     private int menorValor(int primer, int segundo) {
         return primer < segundo ? primer : segundo;
@@ -141,7 +153,6 @@ public class IAPlayer extends Player {
          *	x fila
          *	y columna
          */
-
         //Comprobar vertical
         int ganar1 = 0;
         int ganar2 = 0;
